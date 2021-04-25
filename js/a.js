04974c67e79cfcb364773930608cdf798b122506
1-WebPages-1 <cabraljorge683@gmail.com>
2021-04-25 03:00:34 -0400

    Primer commit guardando los cambios a modificar

---
+++ js/a.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-btn').addEventListener('click', () => 
+{
+    document.querySelector('.Rmen').classList.toggle('show')
+});
+
+
+/* Same effect to te search engine */
+
+document.querySelector('.Sea-Con').addEventListener('click', () => 
+{
+    document.querySelector('.seaN').classList.toggle('seaY')
+});
