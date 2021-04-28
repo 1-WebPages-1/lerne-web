ca33ac61cb88a69050f4a3c251d0d480df1b852c
1-WebPages-1 <cabraljorge683@gmail.com>
2021-04-28 07:35:24 -0400

    modificación a no index y redirección al aceptar politicas

--- js/a.js
+++ js/a.js
@@ -18,13 +18,16 @@ document.querySelector('.Sea-Con').addEventListener('click', () =>
 function compruebaAceptaCookies() {
   if(localStorage.aceptaCookies == 'true'){
     cajacookies.style.display = 'none';
+    window.location.href="./resume.html";
   }
 }
 
 function aceptarCookies() {
   localStorage.aceptaCookies = 'true';
   cajacookies.style.display = 'none';
-}
+  window.location.href="./resume.html";
+  alert("Entrarás en 5s, ¡DALE A ACEPTAR!");
+}setTimeout("aceptarCookies()", 5000);
 
 $(document).ready(function () {
   compruebaAceptaCookies();
